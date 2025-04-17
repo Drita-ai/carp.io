870f80f6364561f7ac95ec31fdfcf3992476fd07
alok <alok.kr300@gmail.com>
2025-04-17 20:55:37 +0530

    Added wiggle animation in tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,7 +7,17 @@ export default {
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 0.3s ease-in-out 3", // shakes 3 times then stops
+      },
+    },
   },
   darkMode: "class",
   plugins: [heroui()],
