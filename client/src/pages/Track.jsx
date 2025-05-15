a95d2bebcb0cea284685a2914d0cfd7d87619896
alok <alok.kr300@gmail.com>
2025-05-15 15:41:37 +0530

    Added button to add carps

--- client/src/pages/Track.jsx
+++ client/src/pages/Track.jsx
@@ -12,6 +12,11 @@ function Track() {
       </div>
 
       <div className="w-[50%] space-y-3 my-5 p-6 rounded-xl shadow-lg bg-[#1a1a1a]">
+        <div className="flex justify-end">
+          <button className="px-4 py-2 text-sm font-medium bg-white text-black border border-neutral-600 rounded-md hover:bg-neutral-400 transition-colors duration-200">
+            Add Carp
+          </button>
+        </div>
         <Cwraps />
       </div>
     </div>
